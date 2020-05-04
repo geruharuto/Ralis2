e42a783c07ddcbea74ffaad0794ac221817934d8
geruharuto <geruharuto2121@yahoo.co.jp>
2020-05-04 15:54:53 +0900

    [Finish]3-11

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '77b23fc4861739cf544ab4af058c1ca02f218c46e218dda86ba97aa3875000768ade734fc321ee053d9308b667ab03f9bfee093f671914cbfc3f8b2050918bd2'
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
  get "todolist/:id/edit" => "todolists#edit", as: "edit_todolist"
  patch "todolists/:id" => "todolists#update", as: "update_todolist"
+
+ delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
